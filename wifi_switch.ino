cfa20f52e04cdaba31bf036a77ad23403be9a853
Pratham Bhatnagar <prathambhatnagar313@gmail.com>
2025-08-31 12:54:36 +0530

    Request and Response added

--- wifi_switch.ino
+++ wifi_switch.ino
@@ -1,12 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
 const char* ssid ="Galaxy";
 const char* password ="bhanu128";
 
 ESP8266WebServer server(80);
-
 
 void setup(){
   Serial.begin(115200);
@@ -20,19 +20,44 @@ void setup(){
     while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(1000);
-
   }
 
   if(WiFi.status() == WL_CONNECTED){
-    Serial.println("Connected !" );
+    Serial.print("Connected to " );
+    Serial.println(WiFi.SSID() );
     Serial.print("IP Address :\t");
     Serial.println(WiFi.localIP());
   }
 
+  if(MDNS.begin("Esp8266")){
+    Serial.println("mDNS Responder Started");
+  }
+  else{
+    Serial.println(" Error mDNS Responder!"); 
+  }
+
+  server.on("/",handleRoot);
+  server.on("/ram",greet);
+  
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP Server Started");
+  
 }
 
 void loop(){
-  Serial.println("Hi From Esp8266");
-  delay(1000);
+  server.handleClient();
 }
 
+void handleRoot(){
+  server.send(200,"text/plain","Hello from ESP");
+}
+
+void handleNotFound(){
+  server.send(404,"text/plain","404 : Requiest Not Found");
+}
+
+void greet(){
+  server.send(200,"text/plain","JAI SHREE RAM");
+}
